2c2bc822552afb9a65b9f697cba99606e42cdd90
Shibster <lagzdinsalbers1@gmail.com>
2023-05-24 10:38:40 +0300

    Objektu vilksana

---
+++ Assets/Skripti/DragDrop.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragDrop : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler{
+	//Uzglaba noradi uz objekti skritpu
+	public Objekti objektuSkripts;
+	private CanvasGroup kanvasGrupa;
+	private RectTransform velkObjRectTransf;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        objektuSkripts.pedejaisVilktais = null;
+        kanvasGrupa.alpha = 0.6f;
+        kanvasGrupa.blocksRaycasts  = false;
+
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        velkObjRectTransf.anchoredPosition += eventData.delta / objektuSkripts.kanva.scaleFactor;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        objektuSkripts.pedejaisVilktais = eventData.pointerDrag;
+        kanvasGrupa.alpha = 1f;
+
+        if(objektuSkripts.vaiIstajaVieta == false)
+        {
+            kanvasGrupa.blocksRaycasts = true;
+        } else {
+            objektuSkripts.pedejaisVilktais = null;
+        }
+        objektuSkripts.vaiIstajaVieta = false;
+    }
+
+    void Start () {
+	//Pieklust velkama objekta canvasgroup komponentei
+	    kanvasGrupa = GetComponent<CanvasGroup>();
+	//Pieklust velkama objekta 
+	//RectTransform komponentei
+		velkObjRectTransf= GetComponent<RectTransform>();
+	}
+
+}
---
+++ Assets/Skripti/Objekti.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objekti : MonoBehaviour {
+	//Uzglabas visus velkmaos objektus
+	public GameObject AtkritumuMasina;
+	public GameObject AtraPalidziba;
+	public GameObject Autobuss;
+
+	//Uzglabas katra transportlidzekla sakotnejo atrasanas vietu
+	[HideInInspector]
+	public Vector2 atkrKoord;
+	[HideInInspector]
+	public Vector2 AtraPalKoord;
+	[HideInInspector]
+	public Vector2 BussKoord;
+
+	public Canvas kanva;
+
+	//Uzglabas audio avoti
+	public AudioSource SkanasAvots;
+	//Uzglabas audio failus
+	public AudioClip[] skanaKoAtskanot;
+
+	[HideInInspector]
+	public bool vaiIstajaVieta = false;
+	public GameObject pedejaisVilktais = null;
+
+	// Use this for initialization
+	void Start () {
+		//Uzsakot speli piefikse kur sakotneji atrodas katra masina
+		atkrKoord = AtkritumuMasina.GetComponent<RectTransform>().localPosition;
+		AtraPalKoord = AtraPalidziba.GetComponent<RectTransform>().localPosition;
+		BussKoord = Autobuss.GetComponent<RectTransform>().localPosition;
+
+	}
+	
+}
