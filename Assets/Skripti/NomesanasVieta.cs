4a481207ed7c02f0f3260569e39e6d2058c666de
Shibster <lagzdinsalbers1@gmail.com>
2023-05-25 10:03:24 +0300

    Realizeta drag and drop funkcija

---
+++ Assets/Skripti/NomesanasVieta.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class NomesanasVieta : MonoBehaviour, IDropHandler {
+	private float vietasZRot, velkObjZRot, rotacijasStarpiba;
+	private Vector2 vietasIzm, velkObjIzm;
+	private float xIzmeruStarp, yIzmeruStarp;
+	public Objekti objektuSkripts;
+	
+	
+	
+	public void OnDrop(PointerEventData eventData)
+    {
+        if(eventData.pointerDrag != null)
+		{
+			if(eventData.pointerDrag.tag.Equals(tag))
+			{
+				//Rotacija
+				vietasZRot = GetComponent<RectTransform>().transform.eulerAngles.z;
+                velkObjZRot = eventData.pointerDrag.GetComponent<RectTransform>().transform.eulerAngles.z;
+				rotacijasStarpiba = Mathf.Abs(velkObjZRot-vietasZRot);
+
+				//Lielums
+				velkObjIzm = eventData.pointerDrag.GetComponent<RectTransform>().localScale;
+                vietasIzm = GetComponent<RectTransform>().localScale;
+
+				xIzmeruStarp = Mathf.Abs(velkObjIzm.x - vietasIzm.x);
+                yIzmeruStarp = Mathf.Abs(velkObjIzm.y - vietasIzm.y);
+
+                Debug.Log("Rotacijas starpiba: "+rotacijasStarpiba+ 
+					"\nx starpiba: "+xIzmeruStarp+ 
+					"\ny starpiba: "+yIzmeruStarp);
+
+				if((rotacijasStarpiba <= 8 || (rotacijasStarpiba >= 354 && rotacijasStarpiba <= 360)) && (xIzmeruStarp <= 0.1 && yIzmeruStarp <= 0.1))
+				{
+					Debug.Log("Nolikts pareizaja vieta");
+					objektuSkripts.vaiIstajaVieta = true;
+					eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+					eventData.pointerDrag.GetComponent<RectTransform>().localRotation = GetComponent<RectTransform>().localRotation;
+					eventData.pointerDrag.GetComponent<RectTransform>().localScale = GetComponent<RectTransform>().localScale;
+
+					switch(eventData.pointerDrag.tag){
+						case "atkritumi":
+							objektuSkripts.SkanasAvots.PlayOneShot(objektuSkripts.skanaKoAtskanot[1]);
+							break;
+
+						case "buss":
+                            objektuSkripts.SkanasAvots.PlayOneShot(objektuSkripts.skanaKoAtskanot[3]);
+                            break;
+
+						case "medicina":
+                            objektuSkripts.SkanasAvots.PlayOneShot(objektuSkripts.skanaKoAtskanot[2]);
+                            break;
+
+						default:
+							Debug.Log("Tags netika atpazits");
+							break;
+					}
+
+				}
+			}else{
+				objektuSkripts.vaiIstajaVieta = false;
+				objektuSkripts.SkanasAvots.PlayOneShot(objektuSkripts.skanaKoAtskanot[0]);
+                switch (eventData.pointerDrag.tag)
+                {
+                    case "atkritumi":
+						objektuSkripts.AtkritumuMasina.GetComponent<RectTransform>().localPosition = objektuSkripts.atkrKoord;
+                        break;
+
+                    case "buss":
+                        objektuSkripts.Autobuss.GetComponent<RectTransform>().localPosition = objektuSkripts.BussKoord;
+                        break;
+
+                    case "medicina":
+                        objektuSkripts.AtraPalidziba.GetComponent<RectTransform>().localPosition = objektuSkripts.AtraPalKoord;
+                        break;
+
+                    default:
+                        Debug.Log("Tags netika atpazits");
+                        break;
+                }
+            }
+		}
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 Drag and drop function being used in a game about dragging vehicles to a correct location
 
 ## Tasks for this project
-- [ ] Ability to move vehicles with the help of the mouse
-- [ ] Ability to place vehicles anywhere on the map
-- [ ] Ability to place the vehicles on a incorrect spot
-- [ ] Ability to place the vehicles in a correct spot
+- [X] Ability to move vehicles with the help of the mouse
+- [X] Ability to place vehicles anywhere on the map
+- [X] Ability to place the vehicles on a incorrect spot
+- [X] Ability to place the vehicles in a correct spot
 - [ ] Rotation changes for the vehicle (Z axis)
 - [ ] Width change by the X axis
 - [ ] Height change by the Y axis
-- [ ] Sound effects
+- [X] Sound effects
